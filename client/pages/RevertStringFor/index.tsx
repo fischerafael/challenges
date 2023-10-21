313ab490e3e5dde86417918d5f76c5d251149438
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-10-21 13:36:05 -0300

    feat: invertendo uma strig like a pro

--- client/pages/RevertStringFor/index.tsx
+++ client/pages/RevertStringFor/index.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+
 export const RevertStringFor = () => {
-  return <div>RevertStringFor</div>;
+  const [string, setString] = useState<string>("");
+
+  const reversedString = reverseStringTypescript(string);
+
+  return (
+    <div>
+      <input
+        placeholder="Type your string"
+        value={string}
+        onChange={(e) => setString(e.target.value)}
+      />
+
+      <div>
+        <p>{reversedString}</p>
+      </div>
+    </div>
+  );
+};
+
+const reverseStringTypescript = (string: string): string =>
+  string.split("").reverse().join("");
+
+const reverseStringWithForLoop = (string: string): string => {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
 };
