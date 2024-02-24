c3902cfc315624a52057a819cf92d4196da77649
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-02-24 13:06:41 -0300

    feat: estados derivados

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const index = () => {
-  return <div>index</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  // estado derivado
+  const fullName = `${firstName} ${lastName}`;
+
+  return (
+    <div>
+      <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      <button>Submit</button>
+
+      <div>{fullName}</div>
+    </div>
+  );
 };
 
 export default index;
